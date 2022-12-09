74a0b044322bfe12bb4706d0f73494bfa83ea5f2
Akinwale Akinjiola <akinjil@gmail.com>
2022-12-09 23:07:12 +0000

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	char alphabets;
 
-	for(alphabets = 'z'; alphabets >= 'a'; alphabets--)
+	for (alphabets = 'z'; alphabets >= 'a'; alphabets--)
 		putchar(alphabets);
 	putchar('\n');
 	return (0);
