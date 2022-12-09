6e00eb5c94d0ca6d1e3eb4e24107bdd88d970f2c
Akinwale Akinjiola <akinjil@gmail.com>
2022-12-09 19:28:20 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,16 +4,19 @@
 #include <stdio.h>
 
 /* betty style doc for function main goes there */
-/**
-* The output of the program returns:
-* The number, followed by
-* if the number is greater than 0: is positive
-* if the number is 0: is zero
-* if the number is less than 0: is negative
-*/
+
 int main(void)
 {
+	/**
+	* The output of the program returns:
+	* The number, followed by
+	* if the number is greater than 0: is positive
+	* if the number is 0: is zero
+	* if the number is less than 0: is negative
+	*/
+	
 	int n;
+	
 	srand(time(0));
  	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
