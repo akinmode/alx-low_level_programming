0262ca44b463b3059bb4fc28ec92d724f13f26b0
Akinwale Akinjiola <akinjil@gmail.com>
2022-12-09 22:51:47 +0000

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	char alphabets;
 
-	for(alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
 		putchar(alphabets);
 	putchar('\n');
 	return (0);
