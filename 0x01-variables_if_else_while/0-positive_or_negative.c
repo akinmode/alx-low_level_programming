79e5d3f5da06355ac140bdd349405a5bda853c00
Akinwale Akinjiola <akinjil@gmail.com>
2022-12-09 19:35:41 +0000

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 	* The number, followed by
 	* if the number is greater than 0: is positive
 	* if the number is 0: is zero
-	* if the number is less than 0: is negative
+	* if the number is less than 0: is negatives
 	*/
 
 	int n;
