000ea4886e38d27446c58eddc99cd9a602c8feb2
akinmode <akinjil@gmail.com>
2022-12-09 23:06:07 +0000

    Tried

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+* main - Checks if a random generated number is negative or positive
+* Description: Checks if a random generated number is negative or positive
+* Return: a string output
+*/
+int main(void)
+{
+	char alphabets;
+
+	for(alphabets = 'z'; alphabets >= 'a'; alphabets--)
+		putchar(alphabets);
+	putchar('\n');
+	return (0);
+}
