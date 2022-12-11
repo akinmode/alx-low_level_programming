6e14f1cc6e98054953fa4763a8f5f752345d6abf
akinmode <akinjil@gmail.com>
2022-12-11 20:28:31 +0000

    Number 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+* main - Checks if a random generated number is negative or positive
+* Description: Checks if a random generated number is negative or positive
+* Return: a string output
+*/
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 48; numbers <= 57; numbers++)
+	{
+		putchar(numbers);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
