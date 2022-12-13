e55b260404b443821e956710b95d4a130a803cf1
akinmode <akinjil@gmail.com>
2022-12-13 20:03:43 +0000

    Update Number 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
 /**
 * print_sign - print a value
-* @c: input parameter
+* @n: input parameter
 * Description: prints all the alphabets in lower case
 * Return: string value
 */
@@ -11,17 +12,17 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
-		putchar(45);
+		_putchar(45);
 		return (-1);
 	}
 	else if (n == 0)
 	{
-		putchar(48);
+		_putchar(48);
 		return (0);
 	}
 	else if (n > 0)
 	{
-		putchar(43);
+		_putchar(43);
 		return (1);
 	}
 	return (0);
