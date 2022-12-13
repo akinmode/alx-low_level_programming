a28444e983ab8d4a1fdb33a708e3c862cc330533
akinmode <akinjil@gmail.com>
2022-12-13 18:45:58 +0000

    Exe 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* print_alphabet_x10 - print a value
+* Desccription: prints all the alphabets in lower case
+* Return: string value
+*/
+
+void print_alphabet(void);
+
+void print_alphabet_x10(void)
+{
+	int i = 1;
+
+	for (i = 1; i <= 10; i++)
+		print_alphabet();
+}
+
+void print_alphabet(void)
+{
+	char alphabets;
+
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+		putchar(alphabets);
+	putchar('\n');
+}
