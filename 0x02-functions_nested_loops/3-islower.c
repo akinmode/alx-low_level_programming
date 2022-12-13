e1b0aced657a99a4eb0ab0275aa7780458d3d06d
Akinwale Akinjiola <akinjil@gmail.com>
2022-12-13 19:12:30 +0000

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,13 +4,14 @@ void print_alphabet(void);
 
 /**
 * _islower - print a value
+* @c: input parameter
 * Desccription: prints all the alphabets in lower case
 * Return: string value
 */
 
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
