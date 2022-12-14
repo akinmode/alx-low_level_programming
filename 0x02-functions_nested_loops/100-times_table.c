813670cbbb4c0d7c46e6fb6bc187661a3709e140
akinmode <akinjil@gmail.com>
2022-12-14 20:48:25 +0000

    next

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
-* times_table - print a value
+* print_times_table - print a value
 * Description: prints all the alphabets in lower case
 * Return: string value
 */
@@ -11,26 +11,34 @@ void print_times_table(int n)
 {
 	int i, x;
 
-	for (i = 0; i <= n; i++)
+	if (n == 0)
 	{
-		for (x = 0; x <= n; x++)
-		{
-			if ((i * x) <= 9)
-			{
-				x == 0 ? printf("%d,", 0) : x < n ?
-				printf("   %d,", i * x) : printf("   %d", i * x);
-			}
-			else if (((i * x) >= 10) && ((i * x) <= 99))
-			{
-				x == 0 ? printf("%d,", 0) : x < n ?
-				printf("  %d,", i * x) : printf("  %d", i * x);
-			}
-			else if (((i * x) >= 100))
-			{
-				x == 0 ? printf("%d,", 0) : x < n ?
-                                printf(" %d,", i * x) : printf(" %d", i * x);
-			}
-		}
+		printf("%d", n);
 		putchar('\n');
+	}
+	else if (n > 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (x = 0; x <= n; x++)
+			{
+				if ((i * x) <= 9)
+				{
+					x == 0 ? printf("%d,", 0) : x < n ?
+					printf("   %d,", i * x) : printf("   %d", i * x);
+				}
+				else if (((i * x) >= 10) && ((i * x) <= 99))
+				{
+					x == 0 ? printf("%d,", 0) : x < n ?
+					printf("  %d,", i * x) : printf("  %d", i * x);
+				}
+				else if (((i * x) >= 100))
+				{
+					x == 0 ? printf("%d,", 0) : x < n ?
+                	                printf(" %d,", i * x) : printf(" %d", i * x);
+				}
+			}
+			putchar('\n');
+		}
 	}
 }
