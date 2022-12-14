4c6b16913771bff0ed8ed7a31f6369994ed6077d
akinmode <akinjil@gmail.com>
2022-12-14 20:38:53 +0000

    Next

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* times_table - print a value
+* Description: prints all the alphabets in lower case
+* Return: string value
+*/
+
+void print_times_table(int n)
+{
+	int i, x;
+
+	for (i = 0; i <= n; i++)
+	{
+		for (x = 0; x <= n; x++)
+		{
+			if ((i * x) <= 9)
+			{
+				x == 0 ? printf("%d,", 0) : x < n ?
+				printf("   %d,", i * x) : printf("   %d", i * x);
+			}
+			else if (((i * x) >= 10) && ((i * x) <= 99))
+			{
+				x == 0 ? printf("%d,", 0) : x < n ?
+				printf("  %d,", i * x) : printf("  %d", i * x);
+			}
+			else if (((i * x) >= 100))
+			{
+				x == 0 ? printf("%d,", 0) : x < n ?
+                                printf(" %d,", i * x) : printf(" %d", i * x);
+			}
+		}
+		putchar('\n');
+	}
+}
