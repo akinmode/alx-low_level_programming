fba2aecb5976ca60ae83f9020de73244509c754b
akinmode <akinjil@gmail.com>
2022-12-15 17:59:23 +0000

    Debugging

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,12 +1,23 @@
-#include "main.h"
+#include <stdio.h>
 
+/* betty style doc for function main goes there */
 /**
-* main - tests function that prints if integer is positive or negative
-* Return: 0
+* main - Checks if a random generated number is negative or positive
+* Description: Checks if a random generated number is negative or positive
+* Return: a string output
 */
-
-int main(void)
+void positive_or_negative(int i)
 {
-	positive_or_negative(0);
-	return (0);
+	if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
 }
