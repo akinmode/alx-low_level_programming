dea06bab779a337cf4005dda46da73e8152588db
akinmode <akinjil@gmail.com>
2022-12-21 18:03:54 +0000

    Added Exercise 6x01

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* _strcat - Concatenate two strings
+* @dest: string one
+* @src : string two
+* Return: a combined string
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	char *str1 = dest;
+	char *str2 = src;
+
+	while (*str1)
+	{
+		str1++;
+	}
+	while (*str2)
+	{
+		*str1 = *str2;
+		str2++;
+		str1++;
+	}
+	*str1 = '\0';
+	return (str1);
+}
