666915e0a30709d69f9f1a5e672d76037a0f66a7
akinmode <akinjil@gmail.com>
2022-12-22 00:21:03 +0000

    Added Exercise 6x04

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,3 +2,28 @@
 #include "main.h"
 
 /**
+* reverse_array - Reverse an array
+* @a: input array
+* @n: the number of elements to swap
+* Return: Return an array
+*/
+
+void reverse_array(int *a, int n)
+{
+	int *p, i, temp, k;
+
+	p = a;
+
+	for (i = 1; i < n; i++)
+	{
+		p++;
+	}
+
+	for (k = 0; k < i / 2; k++)
+	{
+		temp = a[k];
+		a[k] = *p;
+		*p = temp;
+		p--;
+	}
+}
