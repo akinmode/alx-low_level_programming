faa3c23b981fb2bcaabfe58cfab3a8e71bf93914
akinmode <akinjil@gmail.com>
2022-12-22 01:14:24 +0000

    Added Exercise 6x09

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_number - Replace elements of an array
+* @n: input integer
+* Return: Return an array
+*/
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((unsigned int) n % 10 + '0');
+}
