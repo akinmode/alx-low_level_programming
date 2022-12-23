b66b7f16c69c181bf972b7edc59307ca789afab2
Akinwale Akinjiola <akinjil@gmail.com>
2022-12-23 02:42:40 +0000

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,11 +13,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	char *str1 = dest;
 
-<<<<<<< HEAD
-	while (*str1 != 0)
-=======
 	while (*str1 != '\0')
->>>>>>> d8c701982f3412433bb12ce12f7696f33a7882b3
 		str1++;
 	while (n-- > 0)
 		*str1++ = *src++;
