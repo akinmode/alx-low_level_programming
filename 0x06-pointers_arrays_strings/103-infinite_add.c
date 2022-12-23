2f5b05d627aab05f047477fe56bdef20952a7409
akinmode <akinjil@gmail.com>
2022-12-23 02:29:02 +0000

    Added on 12

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -2,20 +2,92 @@
 #include "main.h"
 
 /**
-* print_number - Replace elements of an array
-* @n: input integer
+* infinite_add - Replace elements of an array
+* @n1: input integer
+* @n2: input integer
+* @r: buffer
+* @size_r
 * Return: Return an array
 */
 
-void print_number(int n)
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	if (n < 0)
-	{
-		_putchar('-');
-		n *= -1;
-	}
+	int carry = 0, index = 0, index2;
+	char *s1 = n1, *s2 = n2;
 
-	if (n / 10)
-		print_number(n / 10);
-	_putchar((unsigned int) n % 10 + '0');
+	while (*s1 != '\0')
+		s1++;
+	while (*s2 != '\0')
+		s2++;
+
+	size_r--;
+	r[size_r] = 0;
+	s1--;
+	s2--;
+
+	while (s2 != n2 - 1 && s1 != n1 - 1)
+	{
+		r[index] = *s2 - '0' + *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry++;
+			r[index] -= 10;
+		}
+		index++;
+		s2--;
+		s1--;
+		if (size_r == index && (s1 != n1 - 1 ||
+			s2 != n2 - 1 || carry == 1))
+			return (0);
+	}
+	while (s1 != n1 - 1)
+	{
+		r[index] = *s1 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s1--;
+		index++;
+		if (size_r == index && (s1 != n1 - 1 ||
+			carry == 1))
+			return (0);
+	}
+	while (s2 != n2 - 1)
+	{
+		r[index] = *s2 + carry;
+		carry = 0;
+		if (r[index] > '9')
+		{
+			carry = 1;
+			r[index] -= 10;
+		}
+		s2--;
+		index++;
+		if (size_r == index && (s2 != n2 - 1 ||
+			carry == 1))
+			return (0);
+	}
+	if (carry == 1)
+	{
+		r[index] = '1';
+		r[index + 1] = 0;
+	}
+	else
+	{
+		r[index--] = 0;
+	}
+	index2 = 0;
+	while (index2 <= index)
+	{
+		carry = r[index];
+		r[index] = r[index2];
+		r[index2] = carry;
+		index--;
+		index2++;
+	}
+	return (r);
 }
