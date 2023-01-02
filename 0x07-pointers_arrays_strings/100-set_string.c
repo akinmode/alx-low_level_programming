56b05eaddcb7f14b325f3b40e1c75c2f3a80a428
akinmode <akinjil@gmail.com>
2023-01-02 23:02:20 +0000

    Added Exe 7x8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* set_string - prints the chessboard.
+* @s: input pointer
+* @to: limiter
+* Return: another string
+*/
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
