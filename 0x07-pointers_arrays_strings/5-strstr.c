b3f1d9106ba95c834035eefce064f54a8a15982c
akinmode <akinjil@gmail.com>
2023-01-02 23:40:49 +0000

    Updated 7x5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,39 +2,35 @@
 #include "main.h"
 
 /**
-* _strstr - locates a substring.
-* @haystack: input pointer
-* @needle: input character
-* Return: return a first occurence
+* _strstr - locate a substring
+* @haystack: the string to search
+* @needle: the string to find
+* Return: value
 */
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j, hcount = 0, ncount = 0;
+	int i = 0, j = 0;
 
-	while (haystack[hcount] != '\0')
+	while (haystack[i])
 	{
-		hcount++;
-	}
-	while (needle[ncount] != '\0')
-	{
-		ncount++;
-	}
-
-	for (i = 0; i <= hcount; i++)
-	{
-		for (j = 0; j <= ncount; j++)
+		while (needle[j])
 		{
 			if (haystack[i + j] != needle[j])
 			{
 				break;
 			}
-			else
-			{
-				haystack += i;
-				return (haystack);
-			}
+
+			j++;
 		}
+
+		if (needle[j] == '\0')
+		{
+			return (haystack + i);
+		}
+
+		i++;
 	}
+
 	return ('\0');
 }
