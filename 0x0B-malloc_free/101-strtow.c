291ee1970182c2f17b52ccf19bbca21b1e7ac566
akinmode <akinjil@gmail.com>
2023-01-11 18:30:52 +0000

    Added Exe 0x0Bx6

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -8,13 +8,44 @@
 
 char **strtow(char *str)
 {
-	int strc = 0;
+	int wc = 0, i = 0;
+	char **cstr, *pt = str, delim = ' ';
 
-	if (str == NULL || str == "")
+	if (str == NULL || *str == '\0')
 		return (NULL);
-
-	while (str[strc])
-		strc++;
-
-	return (NULL);
+	while (*pt)
+	{
+		if (!(*pt == delim) && (*(pt + 1) == delim || *(pt + 1) == '\0'))
+			wc++;
+		pt++;
+	}
+	if (wc == 0)
+		return (NULL);
+	cstr = malloc(sizeof(char *) * (wc + 1));
+	if (!cstr)
+		return (NULL);
+	while (*str)
+	{
+		if (*str != delim)
+		{
+			for (pt = str, wc = 0; *pt != ' ' && *pt != 0;)
+				wc++, pt++;
+			cstr[i] = malloc(wc + 1);
+			if (cstr[i] == 0)
+			{
+				while (i >= 0)
+					free(cstr[--i]);
+				free(cstr);
+				return (0);
+			}
+			pt = cstr[i++];
+			while (*str != delim && *str != '\0')
+				*pt++ = *str++;
+			*pt = '\0';
+		}
+		else
+			str++;
+	}
+	cstr[i] = '\0';
+	return (cstr);
 }
