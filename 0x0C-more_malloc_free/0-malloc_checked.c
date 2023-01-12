1c90188a0be17032f321471366a946df9e48ba2c
akinmode <akinjil@gmail.com>
2023-01-12 17:41:00 +0000

    Added Exe 09x00

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+* malloc_checked - allocates memory using malloc.
+* @b: size
+* Return: pointer
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	void *memck;
+
+	memck = malloc(b);
+	if (!memck)
+		exit (98);
+	return (memck);
+}
