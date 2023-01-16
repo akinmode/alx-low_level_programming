92a6b1d39236a8a28bb633a322d5aa5db23a9cc3
Akinwale Akinjiola <akinjil@gmail.com>
2023-01-16 23:24:53 +0000

    Create 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - print name of main file the program was compiled from
+* Return: 0 on success
+*/
+int main(void)
+{
+        printf("%s\n", __FILE__);
+        return (EXIT_SUCCESS);
+}
