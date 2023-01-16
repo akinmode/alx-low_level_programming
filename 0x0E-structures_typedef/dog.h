3110e984158d8ecc573fe033abefb811816c07c2
akinmode <akinjil@gmail.com>
2023-01-16 17:17:19 +0000

    Added 0x0Ex0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _ALX_HEADER
+#define _ALX_HEADER
+/**
+* struct dog - Dog attributes
+* @name: The name of the dog.
+* @age: The age of the dog.
+* @owner: The owner of the dog.
+* Description: The attributes of a dog.
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
