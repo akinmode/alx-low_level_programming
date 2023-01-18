84a4a7e27c8ee32080f1cc68eb218ab3d4a878a5
akinmode <akinjil@gmail.com>
2023-01-18 19:38:30 +0000

    0x0Fx00

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* print_name - function that prints a name.
+* @name: name to print
+* @f: function argument
+* Return: void
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL && name != NULL)
+		(*f)(name);
+}
