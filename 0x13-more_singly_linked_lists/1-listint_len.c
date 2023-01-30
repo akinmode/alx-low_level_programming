4cc72fc42201989029a3e6b85e0f0407c68c64fc
akinmode <akinjil@gmail.com>
2023-01-30 19:14:36 +0000

    Added Exe 0x13x1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* listint_len - function that returns the number of elements in a linked listint_t list.
+* @h: node of the linked  list
+* Return: elements
+*/
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nnodes = 0;
+
+	if (h != NULL)
+	{
+		while (h != NULL)
+		{
+			nnodes++;
+			h = h->next;
+		}
+		return (nnodes);
+	}
+	return (0);
+}
