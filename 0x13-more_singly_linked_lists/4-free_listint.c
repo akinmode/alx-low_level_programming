0416ad9e24aa423fbd205199df11e3fc838a9fa8
akinmode <akinjil@gmail.com>
2023-01-30 23:01:33 +0000

    Added Exe 0x13x4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* free_listint - function that frees a listint_t list.
+* @head: node of the linked  list
+* Return: elements
+*/
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+
+	free(head);
+}
