b3c2ba9de795a8c242c5f1d674e6a8f542210cdc
akinmode <akinjil@gmail.com>
2023-02-02 20:27:12 +0000

    Modified Exe 0x14x00

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,28 +3,29 @@
 /**
 * binary_to_uint - function that converts a binary number to an unsigned int.
 * @b: binary input
-* Return: unsigned integer in decimal */
+* Return: unsigned integer in decimal
+*/
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int res = 0, idx = 0, strc = 0;
+	unsigned int res = 0, idx = 0, strc = 0;
 
-    while (b[strc] != '\0')
-    {
-        strc++;
-    }
+	while (b[strc] != '\0')
+	{
+		strc++;
+	}
 
-    if (b != NULL)
-    {
-        while (strc--)
-        {
-            if (b[strc] != '0' && b[strc] != '1')
-                return (0);
-            if (b[strc] == 49)
-                res += 1 << idx;
-            idx++;
-        }
-        return (res);
-    }
-    return (0);
+	if (b != NULL)
+	{
+		while (strc--)
+		{
+			if (b[strc] != '0' && b[strc] != '1')
+				return (0);
+			if (b[strc] == 49)
+				res += 1 << idx;
+			idx++;
+		}
+		return (res);
+	}
+	return (0);
 }
