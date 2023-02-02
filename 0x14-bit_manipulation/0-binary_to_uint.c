e13f90cebc3e98e9ddf59aa472a96c8889d77da6
akinmode <akinjil@gmail.com>
2023-02-02 20:49:32 +0000

    Modified Exe 0x14x00

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,18 +11,22 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int res = 0, idx = 0, strc = 0;
 
-	while (b[strc])
+	if (b != NULL)
 	{
-		strc++;
-	}
+		while (b[strc])
+		{
+			strc++;
+		}
 
-	while (strc--)
-	{
-		if (b[strc] != '0' && b[strc] != '1')
-			return (0);
-		if (b[strc] == 49)
-			res += 1 << idx;
-		idx++;
+		while (strc--)
+		{
+			if (b[strc] != '0' && b[strc] != '1')
+				return (0);
+			if (b[strc] == '1')
+				res += 1 << idx;
+			idx++;
+		}
+		return (res);
 	}
-	return (res);
+	return (0);
 }
