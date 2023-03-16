48d5dbd5a3afdb7ca77a34cc4f782bfba49fd650
akinmode <akinjil@gmail.com>
2023-03-16 17:33:28 +0000

    Added 0x17x04

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,13 +7,13 @@
 */
 void free_dlistint(dlistint_t *head)
 {
-    dlistint_t *temp;
+	dlistint_t *temp;
 
-    while (head)
-    {
-        temp = head;
+	while (head)
+	{
+		temp = head;
 		head = head->next;
 		free(temp);
-    }
-    free(head);
+	}
+	free(head);
 }
