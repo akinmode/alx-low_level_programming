6fc928887ef88c43f7c3cff7fe20d22b1a1b13be
akinmode <akinjil@gmail.com>
2023-03-16 20:09:57 +0000

    Fixed 0x17x08

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * delete_dnodeint_at_index - deletes the node 
+ * delete_dnodeint_at_index - deletes the node
  * at index index of a dlistint_t linked list.
  * @head: list
  * @index: index to delete
