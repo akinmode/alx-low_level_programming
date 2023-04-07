310ddf64515a4e61bc31ef3034c7ca917e3768f2
akinmode <akinjil@gmail.com>
2023-04-07 21:32:49 +0100

    More fixes 0-hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,16 +8,22 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i = 0;
-	hash_table_t *htable = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t *htable;
 
+	if (size == 0)
+		return (NULL);
+	htable = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (htable == NULL)
 		return (NULL);
 	htable->size = size;
 	htable->array = (hash_node_t **) calloc(htable->size, sizeof(hash_node_t));
-	for (i = 0; i < htable->size; i++)
+	if (htable->array == NULL)
 	{
-		htable->array[i] = NULL;
+		free(htable);
+		return (NULL);
 	}
+	for (i = 0; i < htable->size; i++)
+		htable->array[i] = NULL;
 
 	return (htable);
 }
