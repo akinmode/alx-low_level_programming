269d6c1f30637ff841f15e529272abb3e7396913
akinmode <akinjil@gmail.com>
2023-04-07 23:05:15 +0100

    Fixed 5-hash_table_print

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -22,7 +22,7 @@ void hash_table_print(const hash_table_t *ht)
 			if (comma == 1)
 				printf(", ");
 			comma = 1;
-			printf("'%s' : '%s'", ht->array[i]->key, ht->array[i]->value);
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
 			node = node->next;
 		}
 	}
