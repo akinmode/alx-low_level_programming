ec16e30110fdc4452a48c7f7eb857803a246449c
akinmode <akinjil@gmail.com>
2024-03-21 20:52:53 +0000

    Added 4-free_dlistint

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list.
+ * @head: list
+ * Return: void
+*/
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	free(head);
+}
